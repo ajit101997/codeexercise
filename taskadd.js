57c89c9bfdc9dd8aeb1ee3961c0188a47802fdc8
ajit101997 <81012542+ajit101997@users.noreply.github.com>
2021-03-21 08:12:52 +0000

    Add files via upload

---
+++ taskadd.js
@@ -0,0 +1,21 @@
+function add_record(){
+    var id = document.getElementById("loanId").value;
+    var amount = document.getElementById("loanAmount").value;
+    var loandate = document.getElementById("loanDate").value;
+
+    var xmlObj = new XMLHttpRequest();
+    console.log(xmlObj);
+
+    xmlObj.onreadystatechange =function(){
+        if(xmlObj.readyState==4 && xmlObj.status==200 ){
+            console.log(xmlObj.responseText);
+            document.getElementById("message").innerHTML = "Record Added";
+        }
+    }
+
+    xmlObj.open("post","http://localhost:3000/loanRecord",true);
+    xmlObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var record = `loan_id=${id}&loan_amount=${amount}&loan_date=${loandate}`;
+
+    xmlObj.send(record); 
+}
